98a4a063c09ef533dd786e2919b2c722112a9b2e
ifazia <iguetf@gmail.com>
2024-10-23 15:41:20 +0200

    Création certificat acm

--- acm.tf
+++ acm.tf
@@ -2,6 +2,7 @@
 resource "aws_route53_zone" "petclinicapp" {
   name = "petclinicapp.net"
 }
+
 # Créer le certificat ACM
 resource "aws_acm_certificate" "petclinic_cert" {
   domain_name       = "petclinicapp.net"
@@ -11,11 +12,14 @@ resource "aws_acm_certificate" "petclinic_cert" {
     Name = "petclinic_cert"
   }
 }
+
 # Ajouter l'enregistrement DNS pour la validation du certificat
 resource "aws_route53_record" "petclinic_cert_validation" {
-  name    = aws_acm_certificate.petclinic_cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.petclinic_cert.domain_validation_options[0].resource_record_type
+  for_each = { for idx, val in aws_acm_certificate.petclinic_cert.domain_validation_options : idx => val }
+
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
   zone_id = aws_route53_zone.petclinicapp.zone_id
-  records = [aws_acm_certificate.petclinic_cert.domain_validation_options[0].resource_record_value]
+  records = [each.value.resource_record_value]
   ttl     = 60
 }
