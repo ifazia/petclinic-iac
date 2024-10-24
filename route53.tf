26f52b4c4f5a97a2159962433ec3ce86c1559e96
ifazia <iguetf@gmail.com>
2024-10-24 16:04:26 +0200

    validation certificat acm

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 
 resource "aws_route53_record" "petclinic_cert_validation" {
   zone_id = aws_route53_zone.petclinicapp.zone_id
-  name     = "_d90ec101eea920ec92b0bed129b7d84f.petclinicapp.net"  # Nom fourni par ACM
+  name     = "_d90ec101eea920ec92b0bed129b7d84f.petclinicapp.net."  # Nom fourni par ACM
   type     = "CNAME"
   ttl      = 60
   records  = ["_0e50455f067f74ee3d6fa679ebc31c4a.djqtsrsxkq.acm-validations.aws."]  # Valeur fournie par ACM
