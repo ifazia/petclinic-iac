325b8c8c7a8d22a4626111cde9c3bc137b09446a
ifazia <iguetf@gmail.com>
2024-10-25 16:51:20 +0200

    ajout certificat

--- acm.tf
+++ acm.tf
@@ -14,6 +14,10 @@ resource "aws_acm_certificate" "petclinic_zone" {
   provider          = aws.us-east-1
   domain_name       = var.domain_name
   validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 }
 
 # Enregistrement DNS pour la validation du certificat
@@ -44,9 +48,4 @@ resource "aws_route53_record" "www_namespace" {
   records  = ["petclinicapp.net"]  # Redirige vers petclinicapp.net
 }
 
-# Validation du certificat ACM
-resource "aws_acm_certificate_validation" "petclinic_zone" {
-  provider                = aws.us-east-1
-  certificate_arn         = aws_acm_certificate.petclinic_zone.arn
-  validation_record_fqdns = [for record in aws_route53_record.petclinic_zone : record.fqdn]
-}
+
