5decbabc314b9a2ecd8dce2aa68cb73c79f9ed28
ifazia <iguetf@gmail.com>
2024-10-25 16:24:53 +0200

    ajout records à la zone

--- acm.tf
+++ acm.tf
@@ -2,40 +2,41 @@
 resource "aws_route53_zone" "petclinic_zone" {
   name    = var.domain_name
   comment = "Zone hébergée pour le domaine Petclinic"
+  validation_method = "DNS"
 }
 
 # Zone hébergée pour le domaine
 data "aws_route53_zone" "petclinic_zone" {
   name = aws_route53_zone.petclinic_zone.name
+  private_zone = false
 }
-
 # Certificat ACM pour le domaine
-resource "aws_acm_certificate" "petclinic_cert" {
+resource "aws_acm_certificate" "petclinic_zone" {
   provider          = aws.us-east-1
   domain_name       = var.domain_name
   validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 # Enregistrement DNS pour la validation du certificat
-resource "aws_route53_record" "cert_validation" {
+resource "aws_route53_record" "petclinic_zone" {
   for_each = {
-    for dvo in aws_acm_certificate.petclinic_cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.petclinic_zone.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
       record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
   }
-  zone_id = aws_route53_zone.petclinic_zone.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 300
-  records = [each.value.record]
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.petclinic_zone.zone_id
 }
+
 # Crée dynamiquement www-dev, www-staging, www-production
-resource "aws_route53_record" "www_namespace" {
+resource "aws_route53_record" "petclinic_zone" {
   for_each = toset(var.namespaces)
 
   zone_id = aws_route53_zone.petclinic_zone.zone_id
@@ -43,10 +44,10 @@ resource "aws_route53_record" "www_namespace" {
   type     = "CNAME"
   records  = ["petclinicapp.net"]  # Redirige vers petclinicapp.net
 }
+---
 # Validation du certificat ACM
-resource "aws_acm_certificate_validation" "petclinic_cert_validation" {
+resource "aws_acm_certificate_validation" "petclinic_zone" {
   provider                = aws.us-east-1
-  certificate_arn         = aws_acm_certificate.petclinic_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  certificate_arn         = aws_acm_certificate.petclinic_zone.arn
+  validation_record_fqdns = [for record in aws_route53_record.petclinic_zone : record.fqdn]
 }
-
