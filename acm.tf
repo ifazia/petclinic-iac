841ee356066f2fc98cccbda09ecccf08dc38be9c
ifazia <iguetf@gmail.com>
2024-10-28 13:30:49 +0100

    deploy infrastructure

--- acm.tf
+++ acm.tf
@@ -4,13 +4,8 @@ resource "aws_route53_zone" "petclinic_zone" {
   comment = "Zone hébergée pour le domaine Petclinic"
 }
 
-# Zone hébergée pour le domaine
-data "aws_route53_zone" "petclinic_zone" {
-  name = aws_route53_zone.petclinic_zone.name
-  private_zone = false
-}
 # Certificat ACM pour le domaine
-resource "aws_acm_certificate" "petclinic_zone" {
+resource "aws_acm_certificate" "petclinic_cert" {
   provider          = aws.us-east-1
   domain_name       = var.domain_name
   validation_method = "DNS"
@@ -20,7 +15,7 @@ resource "aws_acm_certificate" "petclinic_zone" {
 }
 
 # Enregistrement DNS pour la validation du certificat
-resource "aws_route53_record" "petclinic_zone" {
+resource "aws_route53_record" "petclinic_validation" {
   for_each = {
     for dvo in aws_acm_certificate.petclinic_zone.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -49,7 +44,7 @@ resource "aws_route53_record" "www_namespace" {
 }
 
 # Validation du certificat ACM
-resource "aws_acm_certificate_validation" "petclinic_zone" {
+resource "aws_acm_certificate_validation" "petclinic_validation" {
   provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.petclinic_zone.arn
    validation_record_fqdns = flatten([
