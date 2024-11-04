b599101e3c6e27a34d183ef5202d3236794fe639
ifazia <iguetf@gmail.com>
2024-11-04 15:55:16 +0100

    origin main

--- backend.tf
+++ backend.tf
@@ -1,15 +1,15 @@
 # Configuration du bucket S3 pour stocker les fichiers d'état
-#resource "aws_s3_bucket" "tfstate" {
-  #bucket = var.s3_bucket_name
+resource "aws_s3_bucket" "tfstate" {
+  bucket = var.s3_bucket_name
 
-  #tags = {
-    #Name = "Petclinic Terraform State Bucket"
-  #}
-#}
+  tags = {
+    Name = "Petclinic Terraform State Bucket"
+  }
+}
 
 # Configuration de la version du bucket S3
 resource "aws_s3_bucket_versioning" "tfstate_versioning" {
-  bucket = var.s3_bucket_name #aws_s3_bucket.tfstate.id
+  bucket = aws_s3_bucket.tfstate.id
   
   versioning_configuration {
     status = "Enabled"
@@ -17,28 +17,28 @@ resource "aws_s3_bucket_versioning" "tfstate_versioning" {
 }
 
 # Table DynamoDB pour le verrouillage de l'état Terraform
-#resource "aws_dynamodb_table" "terraform_locks" {
-  #name         = var.dynamodb_table_name
-  #billing_mode = "PAY_PER_REQUEST"
-  #hash_key     = "LockID"
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-  #attribute {
-    #name = "LockID"
-    #type = "S"
-  #}
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-  #tags = {
-    #Name = "Terraform Lock Table"
-  #}
-#}
-
-# Configuration du backend S3 pour Terraform avec DynamoDB pour le verrouillage
-terraform {
-  backend "s3" {
-    bucket         = var.s3_bucket_name
-    key            = "tfstatefiles/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = var.dynamodb_table_name
-    encrypt        = true
+  tags = {
+    Name = "Terraform Lock Table"
   }
 }
+
+# Configuration du backend S3 pour Terraform avec DynamoDB pour le verrouillage
+#terraform {
+  #backend "s3" {
+    #bucket         = "tfstate-petclinic-bucket"
+    #key            = "tfstatefiles/terraform.tfstate"
+    #region         = "us-east-1"
+    #dynamodb_table = "terraform-state-locks"
+    #encrypt        = true
+  #}
+#}
