f25d49e4de9c25c3ec3ff62e24d40773555913a3
ifazia <iguetf@gmail.com>
2024-11-12 20:50:31 +0100

    deploy infra

--- eks.tf
+++ eks.tf
@@ -142,18 +142,22 @@ resource "helm_release" "kube-prometheus-stack" {
     name  = "grafana.adminPassword"
     value = var.GRAFANA_PASSWORD
   }
+
   set {
     name  = "grafana.ingress.enabled"
     value = "true"
   }
+
   set {
     name  = "grafana.ingress.ingressClassName"
     value = "alb"
   }
+
   set {
     name  = "grafana.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/scheme"
     value = "internet-facing"
   }
+
   set {
     name  = "grafana.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/target-type"
     value = "ip"
@@ -162,11 +166,12 @@ resource "helm_release" "kube-prometheus-stack" {
   # Configuration pour Prometheus
   set {
     name  = "prometheus.prometheusSpec.retention"
-    value = "1d"
+    value = "1d"  # Définit la rétention à 1 jour et à ajuster selon le besoin
   }
+
   set {
     name  = "prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec.resources.requests.storage"
-    value = "2Gi" # à augmenter à 5 Gi
+    value = "10Gi" # à ajuster selon le besoin
   }
 
   # Déployer les règles d'alerte via un ConfigMap
@@ -189,7 +194,42 @@ resource "helm_release" "kube-prometheus-stack" {
                 description = "L'utilisation de la CPU est trop élevée pendant plus de 5 minutes."
               }
             },
-            # Ajouter des règles supplémentaires ici
+            {
+              alert = "HighMemoryUsage"
+              expr  = "sum(container_memory_usage_bytes{container!=\"\",pod!=\"\"}) by (pod) / sum(container_spec_memory_limit_bytes{container!=\"\",pod!=\"\"}) by (pod) > 0.9"
+              for   = "5m"
+              labels = {
+                severity = "critical"
+              }
+              annotations = {
+                summary     = "Le pod {{ $labels.pod }} utilise plus de 90% de mémoire."
+                description = "L'utilisation de la mémoire est trop élevée pendant plus de 5 minutes."
+              }
+            },
+            {
+              alert = "HighHTTPErrorRate"
+              expr  = "sum(rate(http_requests_total{status=\"500\", job=\"spring-petclinic\"}[5m])) by (status) > 0.1"
+              for   = "5m"
+              labels = {
+                severity = "critical"
+              }
+              annotations = {
+                summary     = "Erreur HTTP 500 trop fréquente"
+                description = "Le taux d'erreurs HTTP 500 a dépassé le seuil pendant plus de 5 minutes."
+              }
+            },
+            {
+              alert = "HighNetworkUsage"
+              expr  = "sum(rate(node_network_receive_bytes_total[5m])) by (instance) > 1000000000"
+              for   = "5m"
+              labels = {
+                severity = "critical"
+              }
+              annotations = {
+                summary     = "Utilisation du réseau trop élevée sur {{ $labels.instance }}."
+                description = "L'utilisation du réseau a dépassé 1 Go/s pendant plus de 5 minutes."
+              }
+            }
           ]
         }
       ]
@@ -199,7 +239,6 @@ resource "helm_release" "kube-prometheus-stack" {
   depends_on = [
     kubernetes_service_account.service-account,
     helm_release.alb-controller,
-    module.vpc,
-    helm_release.ebs_csi_driver
+    module.vpc
   ]
 }
