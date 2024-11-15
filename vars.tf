133c4a474bb72b22f1e14847a732027d37c3d7d7
ifazia <iguetf@gmail.com>
2024-11-15 21:12:39 +0100

    destroy infra

--- vars.tf
+++ vars.tf
@@ -62,3 +62,8 @@ variable "private_subnet_cidrs" {
   type    = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
+variable "namespace" {
+  description = "Namespace to deploy the application in"
+  type        = string
+}
